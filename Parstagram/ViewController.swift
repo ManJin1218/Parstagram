41ce958a734200a2b3906c9e85c80324e3afa9af
徐婷 <xuting@nilai.local>
2019-03-21 16:37:46 -0400

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by 徐婷 on 2019/3/21.
+//  Copyright © 2019年 manjin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
